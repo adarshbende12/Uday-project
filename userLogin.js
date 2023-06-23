af0ee31aa74d772491c2bc2e200d52c4a8431b49
adarshbende12 <104903732+adarshbende12@users.noreply.github.com>
2023-06-23 18:01:36 +0530

    Add files via upload

---
+++ userLogin.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+let login=document.getElementsByClassName("submit");
+
+//verify if the given info is vaid using Db's data then
+
+login.onclick=function()
+{
+    window.location.href="dashboard_user.html";
+    console.log(yes);
+}
+
